7808651cdc47a4ef4e3d277ad312adf225d6deab
Philipp Schmitt <philipp@schmitt.co>
2014-02-16 02:28:54 +0100

    Update default config

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return array(
     'piwik_site_id' => '1', // default site id
     'piwik_image_tracking' => false, // Whether to use image tracking instead of javascript
     'piwik_image_page_name' => '', // Pagne name (Image tracking only)
-    'piwik_track_subdomains' => true, // Track visitors across all subdomains
+    'piwik_track_subdomains' => false, // Track visitors across all subdomains
     'piwik_prepend_domain' => true, // Prepend the site domain to the page title when tracking
     'piwik_hide_aliases' =>  true // In the "Outlinks" report, hide clicks to known alias URLs of your domain
 );
